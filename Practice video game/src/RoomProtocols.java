ea549ee071ac2d09e35024694c7f45b5e586d207
AndrewOrdonez <andrewordonez5@gmail.com>
2017-10-05 09:18:04 -0600

    Update RoomProtocols.java

--- Practice video game/src/RoomProtocols.java
+++ Practice video game/src/RoomProtocols.java
@@ -9,6 +9,7 @@ public class RoomProtocols {
 		case 0:run0();break;
 		case 1:run1();break;
 		case 2:run2();break;
+		case 3:run3();break;
 		}
 	}
 	public static void run1(){
@@ -53,8 +54,43 @@ public class RoomProtocols {
 			 Runner.setNewRoom(-1);
 			 Runner.setSpawn(Runner.currentRoom, 1);
 		 }
-	Runner.gravity(450);
+		  else if (DrawPanel.x>1000){
+			  Runner.setNewRoom(1);
+			  Runner.setSpawn(Runner.currentRoom, 0);
+		  }
+		  else if (DrawPanel.x>750){
+			  Runner.gravity(150);
+		  }
+		  else if (DrawPanel.x>300){
+			  Runner.gravity((int) (650-(DrawPanel.x/1.5)));
+		  }
+		  
+		  else{
+			  Runner.gravity(450);
+			  }
 	}
+	public static void run3(){
+		
+		if (DrawPanel.y>1000){
+			Runner.setSpawn(Runner.currentRoom, 0);
+			Runner.dt++;
+		}
+		else if(DrawPanel.y>150){
+			Runner.gravity(2000);
+		}
+	else if(DrawPanel.x<-10){
+			 Runner.setNewRoom(-1);
+			 Runner.setSpawn(Runner.currentRoom, 1);
+		 }
+		  else if(DrawPanel.x>300&&DrawPanel.x<340){
+			  Runner.gravity(2000);
+		  }
+		  else {
+			  Runner.gravity(150);
+		  }
+		  
+	}
+	
 	
 //Big ass list of rooms:
 	static ArrayList<Room> roomList=new ArrayList<Room>();
